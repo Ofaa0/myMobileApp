1c595320c2ab722b4b555e39e3f368b4fa6ba9c1
Ofaa0 <108470120+Ofaa0@users.noreply.github.com>
2024-12-17 18:52:15 +0200

    RunnedMyMobileApp

--- myMobileProject/app/src/main/java/com/example/mymobileproject/AdminMainActivity.java
+++ myMobileProject/app/src/main/java/com/example/mymobileproject/AdminMainActivity.java
@@ -9,7 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminMainActivity extends AppCompatActivity {
 
-    private Button btnViewUsers, btnManageCategories, btnManageProducts, btnLogout;
+    private Button btnViewUsers;
+    private Button btnManageCategories, btnManageProducts, btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
